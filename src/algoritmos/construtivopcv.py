3d5c73e852d7b925aa40da89270aceb6a1cebcda
lorenzo <lorenzomoulin@gmail.com>
2020-07-10 17:38:14 -0300

    implementação do algoritmo de vizinho mais próximo

--- src/algoritmos/construtivopcv.py
+++ src/algoritmos/construtivopcv.py
@@ -1,37 +1,30 @@
 def pcv(dic_instancia):
+    #complexidade de tempo: O(n²)
     """ Esta funcao ira resolver o problema do caixeiro
-        viajante tendo como entrada as matrizes de distancia
-        do problema MCVRPIRFTW interpretando as distancias do
-        arco ij como o custo da viagem.
+    viajante tendo como entrada as matrizes de distancia
+    do problema MCVRPIRFTW interpretando as distancias do
+    arco ij como o custo da viagem.
     """
 
-    copia_dist= []
-    for linha in range(len(dic_instancia["distancia"])):
-        copia_linha = []
-        for item in dic_instancia["distancia"][linha]:
-            copia_linha.append(item)
-        copia_dist.append(copia_linha) #faco uma copia da matriz distancia para fazer alteracoes futuras
-    
-    H = [0]  #H recebe os indices dos nós já na solução (inicia em 0, que é o armz central)
-    L = 0    #L recebe o custo total da viagem até o momento
-    #N recebe os nós que ainda não estão na solucao, lenght de distancia tem o -1 para ignorar o armazem virtual
-    N = list(range(1, len(copia_dist)-1)) #o zero já inicia fora pois já é parte da solucao em H
-    i = 0  #indice do nó corrente
-    
-    while N != []:
-        #obter o menor valor dos arcos que saem de i para j
-        valor_j = min(copia_dist[i][:-1]) #removi o ultimo elemento que se refere ao armazem central virtual
-        j = copia_dist[i][:-1].index(valor_j) #acho o indice de j
-        for k in range(0, len(copia_dist)-1): #faco os nos ja visitados nao aparecerem nos minimos novamente
-            copia_dist[k][j] = 9999
-        H.append(j)
-        if valor_j != 9999:
-            L += valor_j
-        if j in N:
-            N.remove(j)
+    N = len(dic_instancia["distancia"]) #numero de nós no grafo
+    visitados = [True] + (N-1)*[False] #vetor de visitados, False se ainda não foi visitado
+    rota, custo = [0], 0.0
+    N -= 2 # desconto o armazem central e o central virtual
+    i = 0 #armazem central inicial
+
+    while N > 0:
+        min_j = 9999 #menor distância até agora
+        j = 0 #nó mais próximo
+        for k in range(0, len(dic_instancia["distancia"])-1): # pego o nó mais próximo que ainda não foi visitado
+            if dic_instancia["distancia"][i][k] < min_j and not visitados[k]:
+                j, min_j = k, dic_instancia["distancia"][i][k]
+        custo += min_j 
+        rota += [j]
+        visitados[j] = True
+        N -= 1
         i = j
-    if H[len(H)-1] != 0:
-        H.append(0)
-    L += copia_dist[i][len(copia_dist)-1]
+        
+    rota += [0]
+    custo += dic_instancia["distancia"][i][-1] #armazem central virtual, fim da rota
+    return rota, custo
     
-    return H, L
