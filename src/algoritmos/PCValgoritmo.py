50de69831470380632c67ff963677f16c814b7b3
leticia-rafaele <lrafaele22@gmail.com>
2020-07-27 15:54:31 -0300

    Foi colocado todos os prints internos do código como comentário

--- src/algoritmos/PCValgoritmo.py
+++ src/algoritmos/PCValgoritmo.py
@@ -3,32 +3,32 @@ def menordistancia (dic_instancia, configuracoes):
     #Função para calcular um solução viável para o problema do caxeiro viajante utilizando como base o exemplo do vizinho mais proximo
 
     D= dic_instancia["distancia"]
-    print("\n","D: ", D, "\n")
+    #print("\n","D: ", D, "\n")
     visitadas=[4] #cidades que o caxeiro já passou
     custo=0       #Custo da viagem
     N=list(range(0,len(D)-1)) #lista as cidades que faltam ser visitadas
     N.remove(4)                      #Remove a primeira cidade pois é de onde o caxeiro está partindo
     i=4
-    print("Cidades visitadas: ", visitadas)
-    print("Custo da viagem: R$", custo)
-    print("Cidades que ainda precisam ser visitadas: ",N)
-    print("Localização atual: Cidade ", i, "\n")
+    #print("Cidades visitadas: ", visitadas)
+    #print("Custo da viagem: R$", custo)
+    #print("Cidades que ainda precisam ser visitadas: ",N)
+    #print("Localização atual: Cidade ", i, "\n")
     
     while(N!=[]):
-        distancia_corrente=D[i]             #recebe todo os valores referentes as cidades que podem ser visitadas da cidade em que ooo caxeiro esta
-        print("distancias corrente: ", distancia_corrente)
+        distancia_corrente=D[i][:]             #recebe todo os valores referentes as cidades que podem ser visitadas da cidade em que ooo caxeiro esta
+       # print("distancias corrente: ", distancia_corrente)
         valorminimo=min(distancia_corrente) #identifica dentro da lista criada na linha acima qual cidade tem o menor custo de deslocamento
         posi_min=distancia_corrente.index(valorminimo)  #recebe o numero da cidade que tem o custo minimo
-        print("posição do valor minimo: ", posi_min, "\n")
+        #print("posição do valor minimo: ", posi_min, "\n")
  
 
         while(posi_min in visitadas):  #esse comando foi criado para que nenhuma cidade já visitada possa ter seu percurso analisado
             distancia_corrente[posi_min]=valorminimo+9999    #torna o valor minimo encontrado um valor alto fazendo com que seja eliminado da visita
-            print("Novas distancias corrente: ", distancia_corrente)
+            #print("Novas distancias corrente: ", distancia_corrente)
             valorminimo=min(distancia_corrente)
-            print("valorm: ",valorminimo)
+            #print("valorm: ",valorminimo)
             posi_min=distancia_corrente.index(valorminimo)  #recebe a posicao do valor minimo encontrado que foi acrescido com o valor 999
-            print("posi_min: ", posi_min)
+            #print("posi_min: ", posi_min)
     
         #ultimo=[j for j, valor in enumerate(distancia_corrente) if valor==valorminimo] #essa linha faz a comparação entre os valores repetidos e guarda o ultimo analisado
         #posi_min=distancia_corrente.index(ultimo)
@@ -40,14 +40,14 @@ def menordistancia (dic_instancia, configuracoes):
         N.remove(posi_min)              #remove o numero da cidade da lista das que faltam ser visitadas
         i=posi_min                      #recebe o valor da localização atual do caxeiro
 
-        print("Cidades visitadas: ", visitadas)
-        print("Custo da viagem: R$", custo)
-        print("Cidades que ainda precisam ser visitadas: ",N)
-        print("Localização atual: Cidade ", i, "\n")
-    distancia_corrente=D[i]     #recebe os valores da ultima coluna(cidade) para que seja identificado o valor de retorno para a cidade de partida
+        #print("Cidades visitadas: ", visitadas)
+        #print("Custo da viagem: R$", custo)
+        #print("Cidades que ainda precisam ser visitadas: ",N)
+        #print("Localização atual: Cidade ", i, "\n")
+    distancia_corrente=D[i] [:]    #recebe os valores da ultima coluna(cidade) para que seja identificado o valor de retorno para a cidade de partida
     custo+=distancia_corrente[4]   #recebe o valor referente a viagem do ultimo ponto ate o ponto de partida
-    print("Cidades visitadas: ", visitadas)
-    print("Custo total da viagem R$",custo)
-    
-    return dic_instancia #envia a função para o local que for chamada  
+    #print("Cidades visitadas: ", visitadas)
+    #print("Custo total da viagem R$",custo)
+
+    return visitadas, custo #envia a função para o local que for chamada  
 
