b74ff4afe48d01503ba6656e035230e273b5be08
leticia-rafaele <lrafaele22@gmail.com>
2020-07-30 16:22:26 -0300

    Correção do código nas linhas 28 e 59, trocou-se um for por um while

--- src/algoritmos/PCVSimetrico.py
+++ src/algoritmos/PCVSimetrico.py
@@ -6,7 +6,7 @@ def buscalocalcaxeiro(dic_instancia,visitadas,custo):
   distancias= dic_instancia['distancia'] #Busca a matriz de distancias
   #print("distancias: ", distancias)
   solucaoviavel=visitadas[:] #Busca a solução viável encontrada no código do pcv
-  #print('solucaoviavel:',solucaoviavel)
+  #print('solucaoviavel:',solucaoviavel,"\n")
   custos= custo #Busca o custo da solução viável obtida
   #print("custos:",custos)
 
@@ -18,20 +18,21 @@ def buscalocalcaxeiro(dic_instancia,visitadas,custo):
   n=len(solucaoviavel) #guarda o valor do tamanho da lista analisada
   #print('n:',n)
   j=0
+  i=0
 
 
   while melhoria:
     solucao=[]
     melhoria=False
-    for i in range (0,n):
-      if i<=n-2:
-        solucao=solucaoviavel[:]
-        reserva=solucao[i]      #Recebe o valor da primeira cidade encontrada na lista
-        #print("Reserva:", reserva)
-        solucao[i]=solucao[i+1]   #Faz a troca de posição entre duas cidades
-        solucao[i+1]=reserva
-        #print("Solução", i+1, ": ", solucao,"\n")
-        #print('i:',i)
+    #for i in range (0,n):
+    while i<=n-2:
+      solucao=solucaoviavel[:]
+      reserva=solucao[i]      #Recebe o valor da primeira cidade encontrada na lista
+      #print("Reserva:", reserva)
+      solucao[i]=solucao[i+1]   #Faz a troca de posição entre duas cidades
+      solucao[i+1]=reserva
+      #print("Solução", i+1, ": ", solucao)
+      #print('i:',i)
 
       for k in range(0,n):  #Esse bloco recalcula o custo da viagem
         j=k+1
@@ -41,19 +42,21 @@ def buscalocalcaxeiro(dic_instancia,visitadas,custo):
           #print('Novo custo: R$', novocusto)
         else:
           partida=solucao[k]
-          #print('partida:',partida)
+         #print('partida:',partida)
           chegada=solucao[j]
           #print('chegada:',chegada)
           novocusto+=distancias[partida][chegada]  #encontra o valor da viagem utilizando a posicao da cidade de partida e de chegada na matriz
           #print('Novo custo: R$', novocusto)
+      #print('Novo custo: R$', novocusto,"\n")
 
 
       if novocusto<melhorvalor and novocusto>0: #Compara o custo minimo que já havia sido encontrado coom o novo valor obtido com a troca das cidades
         minlocal=solucao
         #print('minlocal:',minlocal,"\n")
         melhorvalor=novocusto
-        #print('melhor valor: ',melhorvalor)
+        #print('melhor valor: ',melhorvalor,"\n")
       novocusto=0
+      i+=1
 
     if custos>melhorvalor:
       solucaoviavel=minlocal
