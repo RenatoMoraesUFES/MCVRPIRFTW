5950b720401e9cd841b57d16790af328ab38082e
leticia-rafaele <lrafaele22@gmail.com>
2021-01-20 08:55:54 -0300

    construtivo adaptado ppara as entradas do henrique

--- src/algoritmos/construtivoparalelo.py
+++ src/algoritmos/construtivoparalelo.py
@@ -1,5 +1,5 @@
 def savings(dic_instancia):
-      #Função que busca minimizar o custo total de transporte obedecendo as restrições de capacidade e de número de visitas a um cliente
+  #Função que busca minimizar o custo total de transporte obedecendo as restrições de capacidade e de número de visitas a um cliente
   D= dic_instancia['distancia'][:] #Busca a matriz distancia 
   capacidade= dic_instancia['num_max_compartimentos']    #Busca o valor da capacidade do veiculo
   #print('capacidade',capacidade) 
@@ -10,14 +10,13 @@ def savings(dic_instancia):
   custo=[]
   rotas=[]
   rotas_unitarias=[]
-  custo_max=0
   
   
   #print(len(D))
-  for i in range(0, len(D)-1):    #Esse bloco calcula o valor economizado com a junção de duas entregas
+  for i in range(0, len(D)):    #Esse bloco calcula o valor economizado com a junção de duas entregas
     linha=[]
     #print('distancia',D[i])
-    for j in range(0,len(D)-1):
+    for j in range(0,len(D)):
       if j!=i and i>0 and j>0:
         linha.append(D[i][0]+D[0][j]-D[i][j])
       else:
@@ -26,16 +25,15 @@ def savings(dic_instancia):
     #print('matriz economias',economias)
   
   for i in range(0,1):            #Esse bloco monta as rotas unitárias com o custo de cada uma e também com o custo da soma de todas as viagens
-    for j in range(1,len(D)-1):
+    for j in range(1,len(D)):
       unitarias=[sum(volume[j]),0,j,0]
       rotas_unitarias.append(unitarias)
-      custo_max+=D[0][j]+D[j][0]      #calcula o custo máximo da soma de todas as rotas
   print('Rotas unitarias:', rotas_unitarias)
 
 
 
-  for i in range(1,len(D)-1):     #Percorrer a matriz economias e colocar as cidades em ordem decrescente de ganhos
-    for j in range(i+1,len(D)-1):
+  for i in range(1,len(D)):     #Percorrer a matriz economias e colocar as cidades em ordem decrescente de ganhos
+    for j in range(i+1,len(D)):
       custo=economias[i][j]
       rotas.append([custo,(i,j)])
   rotas=sorted(rotas,reverse=True)[:] #colocou cada linha em ordem decrescente de valor
@@ -47,30 +45,26 @@ def savings(dic_instancia):
     i=cidade[1][0]   #seleciona a cidade de partida da rota mais economica
     j=cidade[1][1]   #seleciona a cidade de chegada da rota mais economica
     
-    verificacao_rota=restricao_de_rotas(i,j,rotas_unitarias)
-    print('restrição de rota',verificacao_rota)
-    if verificacao_rota==True:
-      verificacao_demanda= restricao_de_demanda(i,j,capacidade,volume,rotas_unitarias)
-      print('RESTRIÇÃO DE DEMANDA',verificacao_demanda)
-      if verificacao_demanda==True:
+    
+    verificacao_demanda= restricao_de_demanda(i,j,capacidade,volume,rotas_unitarias)
+    print('RESTRIÇÃO DE DEMANDA',verificacao_demanda)
+    if verificacao_demanda==True:
+      verificacao_rota=restricao_de_rotas(i,j,rotas_unitarias)
+      print('restrição de rota',verificacao_rota)
+      if verificacao_rota==True:
         rotas_unitarias=unir_rotas(i,j,rotas_unitarias,volume)
-    #for cidade in rotas_unitarias:
-    #  if i in cidade:
-    #    rotas_unitarias.remove(cidade)    #Remove a cidade inserida das rotas unitárias
-    #  if j in cidade:
-    #    rotas_unitarias.remove(cidade)    #Remove a cidade inserida das rotas unitárias
-    #rotas_unitarias=rota_corrente
-    #verificacao_demanda= restricao_de_demanda(i,j,capacidade,volume,rotas_unitarias)
-    #if verificacao_demanda==True:
-      #rota_corrente=unir_rotas(i,j,rotas_unitarias,volume)
-      #verificacao_rota=restricao_de_rotas(i,j,rota_corrente,rotas_unitarias)
-      #if verificacao_rota==True:
-        #rota_corrente=unir_rotas(i,j,rota_corrente,rotas_unitarias,volume)
-    #print('rota unitária nova',rotas_unitarias)
-    #print("Rota Corrente",rota_corrente)
+
+  custo_final=[]
+  for rota in rotas_unitarias: #calcula o custo final de cada rota
+    custo_rota=0
+    for i in range(1,len(rota)-1):
+      custo_rota+=D[rota[i]][rota[i+1]]   #lista parcial de custos
+    custo_final.append(custo_rota)    #adiciona o custo da rota completa na lista final de custos
+
   
   
   print('Rotas unitárias:',rotas_unitarias)
+  print('custos finais',custo_final)
   
   return rotas_unitarias
 
@@ -82,10 +76,9 @@ def unir_rotas(i, j,rotas_unitarias,volume):
   print('j',j)
   print('!!!!rotas unitárias:',rotas_unitarias)
 
-  cont=0
+
   for rota in rotas_unitarias: 
     
-    #if i==rota[2] and i==rota[-2]:       #verifica se a cidade que já pertence a rota é um extremo
 
     if i in rota and j not in rota:   #verifica se uma das cidades dessa rota já pertencee a rota corrente
       if i==rota[2]:       #verifica se a cidade que já pertence a rota é um extremo
@@ -94,6 +87,7 @@ def unir_rotas(i, j,rotas_unitarias,volume):
         for cidade in rotas_unitarias:
           if j in cidade and j==cidade[2] and j==cidade[-2]:
             rotas_unitarias.remove(cidade)    #Remove a cidade inserida das rotas unitárias
+
       elif i==rota[-2]:      #verifica se a cidade que já pertence a rota é um extremo
         rota.insert(-1,j)       #adiciona somente a cidade que não faz parte da rota ainda
         rota[0]=rota[0]+sum(volume[j])    #Adiciona ao volume da rota a demanda da nova cidade
@@ -102,7 +96,7 @@ def unir_rotas(i, j,rotas_unitarias,volume):
             rotas_unitarias.remove(cidade)      #Remove a cidade inserida das rotas unitárias
       else:
         return rotas_unitarias
-      #elif j==rota[2] and j==rota[-2]:  
+ 
     elif j in rota and i not in rota:
       if j==rota[2]:         #verifica se a cidade que já pertence a rota é um extremo
         rota.insert(2,i)     #adiciona somente a cidade que não faz parte da rota ainda
@@ -151,8 +145,8 @@ def restricao_de_demanda(i, j,capacidade,volume,rotas_unitarias):
   print('i',i)
   print('j',j)
   print('ROTA UNITARIA******',rotas_unitarias)
-  #demanda_corrente=sum(volume[i])+sum(volume[j])    #soma-se a demanda das cidades que estãotentando entrar na rota para verificara  capacidade
-  for rota in rotas_unitarias:  
+
+  for rota in rotas_unitarias:  #Calcula a demanda das cidades candidatas e da possivel rota que já estão inseridas
     
     if i in rota and j not in rota:
       demanda_corrente+=rota[0]
@@ -160,30 +154,8 @@ def restricao_de_demanda(i, j,capacidade,volume,rotas_unitarias):
       demanda_corrente+=rota[0]
       
   print('DEMANDA CORRENTE',demanda_corrente)
-  if demanda_corrente<=capacidade:
+  if demanda_corrente<=capacidade:  #verifica se a demanda atende a restrição de capacidade
     return True
   else:
     return False
     
-
-
-
-
-'''    if i in rota and j not in rota:   #verifica se uma das cidades dessa rota já pertencee a rota corrente
-      if i==rota[2] or i==rota[-2]:       #verifica se a cidade que já pertence a rota é um extremo
-        demanda_corrente=rota[0]+sum(volume[j])   #soma-se ao valor da rota corrente o volume da nova cidade
-        print('DEMANDA CORRENTE',demanda_corrente)
-        if demanda_corrente<=capacidade:    # ele testa se a demanda existente a a da cidade candidata a rota nao excedem a capacidade maxima
-          return True  # se nao exceder, entao SIM. as rotas podem ser unidas
-        else:
-          return False
-      else:
-        return False
-    if j in rota and i not in rota:   #verifica se uma das cidades dessa rota já pertencee a rota corrente
-      if j==rota[2] or j==rota[-2]:       #verifica se a cidade que já pertence a rota é um extremo
-        demanda_corrente=rota[0]+sum(volume[i])   #soma-se ao valor da rota corrente o volume da nova cidade
-        print('DEMANDA CORRENTE',demanda_corrente)
-        if demanda_corrente<=capacidade:    # ele testa se a demanda existente a a da cidade candidata a rota nao excedem a capacidade maxima
-          return True  # se nao exceder, entao SIM. as rotas podem ser unidas
-        else:
-          return False'''
