b00c2274c080964eefff750e31d98c51e967c30c
leticia-rafaele <lrafaele22@gmail.com>
2021-07-08 02:21:50 -0300

    Função Saída e melhorias

--- src/algoritmos/construtivoparalelo.py
+++ src/algoritmos/construtivoparalelo.py
@@ -2,24 +2,14 @@ def savings(dic_instancia):
       #Função que busca minimizar o custo total de transporte obedecendo as restrições de capacidade e de número de visitas a um cliente
   D= dic_instancia['distancia'][:] #Busca a matriz distancia 
   #capacidade= dic_instancia['num_max_compartimentos']    #Busca o valor da capacidade do veiculo
-  #print('capacidade',capacidade) 
   prop=dic_instancia['caixas_do_cliente']
-  #print('caixas_cliente',prop)
   volume=dic_instancia['volume_caixa']      #Busca os valores das demandas
-  #print('distancia',D)
-  #print('Demanda',volume)
-  volume_limite=100
+  volume_limite=50
   nosf=dic_instancia['facilidades']
+  cd=dic_instancia['custo_km_rodado']
+  ck=dic_instancia['custo_fixo_bike']
   economias=[]
-  custo=[]
-  rotas=[]
-  rotas_unitarias=[]
-  volume_por_rota_unitaria=[]
-  volume_final=[]
-  unitarias=[]
-  custo_final=[]
   
-  #print(len(D))
   for i in range(0,len(D)-1):    #Esse bloco calcula o valor economizado com a junção de duas entregas
     linha=[]
     for j in range(0,len(D)-1):
@@ -29,9 +19,9 @@ def savings(dic_instancia):
         linha.append(9999)    # matriz savings deve ser a subtração da primeira com a segunda
     economias.append(linha)
   
-  #print('matriz economias',economias)
 
   #Cria o vetor de volumes calculados por cliente
+  volume_por_rota_unitaria=[]
   for i in range(0,len(prop)-1-nosf):
     
     volume_cliente=0
@@ -39,24 +29,25 @@ def savings(dic_instancia):
       if prop[i][j]==1:
         volume_cliente+=volume[j]
     volume_por_rota_unitaria.append(volume_cliente)
-  #print('volume por rota unitaria',volume_por_rota_unitaria)
-  
-  
-  for i in range(0,1):            #Esse bloco monta as rotas unitárias com o custo de cada uma e também com o custo da soma de todas as viagens
+
+  #Esse bloco monta as rotas unitárias com o custo de cada uma e também com o custo da soma de todas as viagens
+  unitarias=[]
+  rotas_unitarias=[]
+  for i in range(0,1):            
     for j in range(1,len(D)-2-nosf):
-      #print('lend',len(D))
       unitarias=[j]
       rotas_unitarias.append(unitarias)
-  #print('Rotas unitarias:', rotas_unitarias)
 
 
-
-  for i in range(1,len(D)-2-nosf):     #Percorrer a matriz economias e colocar as cidades em ordem decrescente de ganhos
+  #Percorrer a matriz economias e colocar as cidades em ordem decrescente de ganhos
+  rotas=[]
+  custo=[]
+  for i in range(1,len(D)-2-nosf):     
     for j in range(i+1,len(D)-2-nosf):
       custo=economias[i][j]
       rotas.append([custo,(i,j)])
   rotas=sorted(rotas,reverse=True)[:] #colocou cada linha em ordem decrescente de valor
-  #print('Economias:',rotas)
+
 
   for cidade in rotas:   #percorre cada rota em ordem decrescente de economias
     
@@ -77,22 +68,42 @@ def savings(dic_instancia):
       if verificacao_demanda==True:
         rotas_unitarias=unir_rotas(i,j,rotas_unitarias)
 
-  for rota in rotas:
-    rota.insert(0, 0)
-    rota.append(0)
+  #CALCULA O VOLUME QUE CADA ROTA CARREGA, COMO TAMBÉM O CUSTO DE CADA UMA
+  custo_final=[]
+  volume_final=[]
+  cdr_final=[]
   for rota in rotas_unitarias:
     demanda_rota=0
     custo_rota=0
+    cdr=0
     for cidade in rota:
       demanda_rota+=volume_por_rota_unitaria[cidade-1]
       custo_rota+=D[cidade][cidade+1]
-      #print('custo rota',custo_rota)
+      cdr=custo_rota*cd[1]            #calcula o custo do km para bicicleta convencional
     volume_final.append(demanda_rota)
     custo_final.append(custo_rota)
+    cdr_final.append(cdr)
 
   rotas_formadas=rotas_unitarias
   
-  custo_final = []
+  #Busca as caixas de cada cliente e seus respectivos volumes quue estarão em cada rota
+  caixas_por_rota=[]
+  r=[]
+  caixa=()
+  for rota in rotas_formadas:
+    for cidade in rota:
+      for j in range(0, len(prop[cidade])):
+        if prop[cidade-1][j]==1:
+          caixa=(cidade,j,volume[j])
+          r.append(caixa)
+        caixa=()
+    caixas_por_rota.append(r)
+    r=[]
+
+  #print('caixas por rota',caixas_por_rota)
+
+  #Insere o armazem no inicio e final das rotas formadas, para padronização, e calcula o custo de cada uma delas
+  custo_final = []            
   for rota in rotas_formadas:
     custo_corrente = 0
     rota.insert(0,0)
@@ -101,21 +112,29 @@ def savings(dic_instancia):
       custo_corrente += D[rota[i]][rota[i+1]]
     custo_corrente += D[rota[-2]][-1]
     custo_final.append(custo_corrente)
-  #a diferença de valor encontrada na rota que inicia com a cidade 6 é devido a ordem de visita da rota
+  custo_total=sum(custo_final)    #Calcula o custo final de todas as rotas
+  custo_utilizacao=len(rotas_formadas)*ck[1] #custo de utilização
+  #print('custo utilização',custo_utilizacao)
+  #Calcula o custo por km rodado para cada rota
 
-  #print('Rotas formadas:',rotas_formadas)
-  #print('custos finais',custo_final)
-  #print('Volumes finais',volume_final)
+
+
   dic_solucao = {
-    'caminho'   : rotas_formadas,
-    'volume'    : volume_final,
-    'custo'     : custo_final
-          }
+    'Caminho'           : rotas_formadas,
+    'Volume'            : volume_final,
+    'Custo'             : custo_final,
+    'Caixas por rota'   : caixas_por_rota,
+    'Custo Total'       : custo_total,
+    'Custo_km_rodado_rotas'   : cdr_final,        # custo por km rodado por rota (lista float)
+    'Custo_utilização'   : custo_utilizacao
+    }
+    
+
   return dic_solucao
   
 
 
-   
+#FUNÇÃO PARA UNIR AS ROTAS VIÁVEIS
 def unir_rotas(i, j, rotas_unitarias):
   '''p1, p2 = [], []
   for rota in rotas:
@@ -159,13 +178,7 @@ def unir_rotas(i, j, rotas_unitarias):
   
 
 
-
-
-
-
-
-
-
+#FUNÇÃO QUE VERIFICA SE CONSEGUE COLOCAR DOIS CLIENTES EM UMA ROTA ANALISANDO O VOLUME DEMANDADO POR CADA UM E A CAPACIDADE DO VEÍCULO
 def restricao_de_demanda(i, j, volume_limite,volume,rotas_unitarias):
   for rota in rotas_unitarias:  #Calcula a demanda das cidades candidatas e da possivel rota que já estão inseridas
     if i in rota and j not in rota:
@@ -191,7 +204,7 @@ def restricao_de_demanda(i, j, volume_limite,volume,rotas_unitarias):
       else:
         return False
 
-
+#FUNÇÃO QUE VERIFICA SE CONSEGUE COLOCAR DOIS CLIENTES EM UMA ROTA ANALISANDO A SEQUENCIA DE ENTREGA 
 def restricao_de_rotas(i, j,rotas_unitarias):
   #print('i',i)
   #print('j',j)
